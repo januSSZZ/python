4070b691b36002930e59f09db225a179fbc87a3e
januSSZZ <jankub225@gmail.com>
2025-01-15 08:29:35 +0100

    Add files via upload
    good

---
+++ JĘ.py
@@ -0,0 +1,6 @@
+
+imie = input( " podaj swoje imie ")
+nazwisko = input(" podaj swoje nazwisko ")
+print(f" Witaj , {imie} {nazwisko}")
+wiek = int(input("ile masz lat " ))
+print(f"do pełnoletności brakuje ci lat {18-wiek}")
