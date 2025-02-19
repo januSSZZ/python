3353e858873a6b1db63e8bf2fa18876be870df76
januSSZZ <jankub225@gmail.com>
2025-02-19 08:16:46 +0100

    Add files via upload

---
+++ ph.py
@@ -0,0 +1,12 @@
+l = 100
+l7 = 0
+l3 = 0
+l37 = 0
+print('podzielna przez 7 i przez 3:')
+for n in range(1,l+1):
+    if n%7==0 and n%3==0:
+        print(n)
+print('podzielna przez 3:')
+for n in range(1,l+1):
+    if n%3==0:
+        print(n)
